4bc0f2221595b38f96b4273f562171c8e55b6203
MicFin <mrfinneran@gmail.com>
2018-07-17 20:54:15 -0400

    Update tests for lab with new paragraph

--- spec/lab.spec.js
+++ spec/lab.spec.js
@@ -4,31 +4,22 @@ const chai = require('chai')
 const expect = chai.expect
 
 const paragraph =
-  'We the People of the United States, in Order to form a more ' +
-  'perfect Union, establish Justice, insure domestic Tranquility, ' +
-  'provide for the common defence, promote the general Welfare, and ' +
-  'secure the Blessings of Liberty to ourselves and our Posterity, ' +
-  'do ordain and establish this Constitution for the United States ' +
-  'of America.'
+  'When in the Course of human events it becomes necessary for one ' +
+  'people to dissolve the political bands which have connected them ' +
+  'with another and to assume among the powers of the earth the ' +
+  'separate and equal station to which the Laws of Nature and of ' +
+  'Natures God entitle them a decent respect to the opinions of ' +
+  'mankind requires that they should declare the causes which impel ' +
+  'them to the separation'
 
 const collFuncs = require('../lib/lab.js')
 
 describe('lab', () => {
   describe('normalized words', () => {
-    const normalizedWords = [
-      'A', 'AMERICA', 'AND', 'AND', 'AND', 'BLESSINGS', 'COMMON',
-      'CONSTITUTION', 'DEFENCE', 'DO', 'DOMESTIC', 'ESTABLISH',
-      'ESTABLISH', 'FOR', 'FOR', 'FORM', 'GENERAL', 'IN',
-      'INSURE', 'JUSTICE', 'LIBERTY', 'MORE', 'OF', 'OF', 'OF',
-      'ORDAIN', 'ORDER', 'OUR', 'OURSELVES', 'PEOPLE', 'PERFECT',
-      'POSTERITY', 'PROMOTE', 'PROVIDE', 'SECURE', 'STATES',
-      'STATES', 'THE', 'THE', 'THE', 'THE', 'THE', 'THE', 'THIS',
-      'TO', 'TO', 'TRANQUILITY', 'UNION', 'UNITED', 'UNITED',
-      'WE', 'WELFARE'
-    ]
+    const normalizedWords = paragraph.split(' ').map(word => word.toUpperCase())
 
     it('has the correct length', () => {
-      expect(collFuncs.getNormalizedWords(paragraph).length).to.equal(52)
+      expect(collFuncs.getNormalizedWords(paragraph).length).to.equal(71)
     })
 
     it('matches expected array', () => {
@@ -38,33 +29,25 @@ describe('lab', () => {
   })
 
   describe('unique words', () => {
-    const sortedUniqueWords = [
-      'A', 'AMERICA', 'AND', 'BLESSINGS', 'COMMON',
-      'CONSTITUTION', 'DEFENCE', 'DO', 'DOMESTIC', 'ESTABLISH',
-      'FOR', 'FORM', 'GENERAL', 'IN', 'INSURE', 'JUSTICE',
-      'LIBERTY', 'MORE', 'OF', 'ORDAIN', 'ORDER', 'OUR',
-      'OURSELVES', 'PEOPLE', 'PERFECT', 'POSTERITY', 'PROMOTE',
-      'PROVIDE', 'SECURE', 'STATES', 'THE', 'THIS', 'TO',
-      'TRANQUILITY', 'UNION', 'UNITED', 'WE', 'WELFARE'
-    ]
+    const uniqueWords = ["WHEN", "IN", "THE", "COURSE", "OF", "HUMAN", "EVENTS", "IT", "BECOMES", "NECESSARY", "FOR", "ONE", "PEOPLE", "TO", "DISSOLVE", "POLITICAL", "BANDS", "WHICH", "HAVE", "CONNECTED", "THEM", "WITH", "ANOTHER", "AND", "ASSUME", "AMONG", "POWERS", "EARTH", "SEPARATE", "EQUAL", "STATION", "LAWS", "NATURE", "NATURES", "GOD", "ENTITLE", "A", "DECENT", "RESPECT", "OPINIONS", "MANKIND", "REQUIRES", "THAT", "THEY", "SHOULD", "DECLARE", "CAUSES", "IMPEL", "SEPARATION"]
 
     it('has the correct length', () => {
-      expect(collFuncs.getUniqueWords(paragraph).length).to.equal(38)
+      expect(collFuncs.getUniqueWords(paragraph).length).to.equal(49)
     })
 
     it('matches expected array', () => {
       expect(collFuncs.getUniqueWords(paragraph).sort())
-        .to.deep.equal(sortedUniqueWords)
+        .to.deep.equal(uniqueWords.sort())
     })
   })
 
   describe('word count', () => {
     it('has the correct word count', () => {
-      expect(collFuncs.wordCount(paragraph)).to.equal(52)
+      expect(collFuncs.wordCount(paragraph)).to.equal(71)
     })
 
     it('has the correct unique word count', () => {
-      expect(collFuncs.wordCount(paragraph, true)).to.equal(38)
+      expect(collFuncs.wordCount(paragraph, true)).to.equal(49)
     })
   })
 })
